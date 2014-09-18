0dba9d433f9157f5dac70921e4f4fdda6b534cc7
ivan <peij@163.com>
2014-09-18 16:09:37 +0800

    first

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,101 +23,101 @@ class HomeController < ApplicationController
   def oauth
     code = params[:code]
 
-    # response = RestClient.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid=#{app_id}&secret=#{app_secret}&code=#{code}&grant_type=authorization_code")
-    # response = JSON.parse(response)
-    # # p response
-    #
-    # access_token = response['access_token']
-    # refresh_token = response['refresh_token']
-    # @openid = response['openid']
-    #
-    # user = User.where(openid: @openid).first
-    #
-    #
-    # if user.present?
-    #
-    #   if user.last_get_time < Time.now.beginning_of_day
-    #     user.today_gua_count = user.today_gua_count + Setting.every_day_gua
-    #     user.last_get_time = Time.now
-    #     user.save
-    #   end
-    #
-    # else
-    #
-    #   userinfo_url = "https://api.weixin.qq.com/sns/userinfo?access_token=#{access_token}&openid=#{@openid}&lang=zh_CN"
-    #   response = RestClient.get(userinfo_url)
-    #
-    #   if response.code == 200
-    #     response = JSON.parse(response)
-    #     p response
-    #     nickname = response['nickname']
-    #     sex = response['sex']
-    #     province = response['province']
-    #     city = response['city']
-    #     country = response['country']
-    #     headimgurl = response['headimgurl']
-    #
-    #     opt={}
-    #     opt[:access_token] = access_token
-    #     opt[:refresh_token] = refresh_token
-    #     opt[:openid] = @openid
-    #     opt[:nickname] = nickname
-    #     opt[:sex] = sex
-    #     opt[:province] = province
-    #     opt[:city] = city
-    #     opt[:country] = country
-    #     opt[:headimgurl] = headimgurl
-    #     opt[:today_gua_count] = Setting.every_day_gua
-    #     opt[:last_get_time] = Time.now
-    #     user = User.new(opt)
-    #     user.save
-    #
-    #   end
-    #
-    # end
-    #
-    #
-    # mid = params[:mid]
-    # # 用户分享过来的
-    # if mid.present?
-    #   share_user = User.where(openid: mid).first
-    #   if share_user.present?
-    #
-    #     opt = {}
-    #     opt[:share_user_id] = share_user.id
-    #     opt[:click_user_id] = user.id
-    #     opt[:share_time] = Time.now.strftime('%Y%m%d').to_i
-    #     # 如果当前不存在,才增加
-    #     unless Share.exists?(opt)
-    #       share = Share.new(opt)
-    #       share.save
-    #
-    #       # 重新计算当天的剩余抽奖次数
-    #       if share_user.last_get_time < Time.now.beginning_of_day
-    #         share_user.today_gua_count = Setting.every_day_gua + 1
-    #       else
-    #         share_user.today_gua_count = share_user.today_gua_count + 1
-    #       end
-    #       share_user.last_get_time = Time.now
-    #
-    #       friend = Friend.new(user_id: share_user.id, friend_id: user.id)
-    #       if friend.save
-    #         share_user.hy_count = share_user.hy_count + 1
-    #       end
-    #       share_user.save
-    #
-    #     end
-    #
-    #   end
-    #
-    # end
-    #
-    # # 默认次数
-    # @gua_count = user.today_gua_count
+    response = RestClient.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid=#{app_id}&secret=#{app_secret}&code=#{code}&grant_type=authorization_code")
+    response = JSON.parse(response)
+    # p response
 
-    @openid = 'ozmMMt8Irb8htU_jlRVX3Ekkzov8'
+    access_token = response['access_token']
+    refresh_token = response['refresh_token']
+    @openid = response['openid']
+
     user = User.where(openid: @openid).first
+
+
+    if user.present?
+
+      if user.last_get_time < Time.now.beginning_of_day
+        user.today_gua_count = user.today_gua_count + Setting.every_day_gua
+        user.last_get_time = Time.now
+        user.save
+      end
+
+    else
+
+      userinfo_url = "https://api.weixin.qq.com/sns/userinfo?access_token=#{access_token}&openid=#{@openid}&lang=zh_CN"
+      response = RestClient.get(userinfo_url)
+
+      if response.code == 200
+        response = JSON.parse(response)
+        p response
+        nickname = response['nickname']
+        sex = response['sex']
+        province = response['province']
+        city = response['city']
+        country = response['country']
+        headimgurl = response['headimgurl']
+
+        opt={}
+        opt[:access_token] = access_token
+        opt[:refresh_token] = refresh_token
+        opt[:openid] = @openid
+        opt[:nickname] = nickname
+        opt[:sex] = sex
+        opt[:province] = province
+        opt[:city] = city
+        opt[:country] = country
+        opt[:headimgurl] = headimgurl
+        opt[:today_gua_count] = Setting.every_day_gua
+        opt[:last_get_time] = Time.now
+        user = User.new(opt)
+        user.save
+
+      end
+
+    end
+
+
+    mid = params[:mid]
+    # 用户分享过来的
+    if mid.present?
+      share_user = User.where(openid: mid).first
+      if share_user.present?
+
+        opt = {}
+        opt[:share_user_id] = share_user.id
+        opt[:click_user_id] = user.id
+        opt[:share_time] = Time.now.strftime('%Y%m%d').to_i
+        # 如果当前不存在,才增加
+        unless Share.exists?(opt)
+          share = Share.new(opt)
+          share.save
+
+          # 重新计算当天的剩余抽奖次数
+          if share_user.last_get_time < Time.now.beginning_of_day
+            share_user.today_gua_count = Setting.every_day_gua + 1
+          else
+            share_user.today_gua_count = share_user.today_gua_count + 1
+          end
+          share_user.last_get_time = Time.now
+
+          friend = Friend.new(user_id: share_user.id, friend_id: user.id)
+          if friend.save
+            share_user.hy_count = share_user.hy_count + 1
+          end
+          share_user.save
+
+        end
+
+      end
+
+    end
+
+    # 默认次数
     @gua_count = user.today_gua_count
+
+    # @openid = 'ozmMMt8Irb8htU_jlRVX3Ekkzov8'
+    # user = User.where(openid: @openid).first
+    # @gua_count = user.today_gua_count
   end
 
 
